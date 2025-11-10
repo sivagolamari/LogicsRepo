78c2f5070cae007a556eb9d1df4212cf738f0595
sivagolamari <sivareddygolamari0@gmail.com>
2025-11-10 08:36:29 +0530

    Implemented addition Logic

--- Demo.java
+++ Demo.java
@@ -1,4 +1,13 @@
+import java.util.*;
 public class Demo{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number");
+		int n1 = sc.nextInt();
 
+		System.out.println("Enter Second Number");
+		int n2 = sc.nextInt();
+		System.out.println("Sum of n1 and n2 ::"+(n1+n2));
+	}
 }
 
